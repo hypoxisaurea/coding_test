3c9deb377e1325fc2b39512c4b8153370575ff94
Bae Soyeon <flickerlight14@naver.com>
2025-10-14 15:59:04 +0900

    Title: 3N + 1 수열, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251014/3N + 1 수열/3n-plus-1-sequence.py
@@ -0,0 +1,16 @@
+n = int(input())
+count = 0
+
+while True:
+    if n == 1:
+        break
+    
+    if n % 2 == 0:
+        n //= 2
+    else:
+        n = n * 3 + 1
+
+    count += 1
+
+
+print(count)
