db568059238e7d6c4a3dc9846e552ead672bd272
Bae Soyeon <flickerlight14@naver.com>
2025-10-17 12:06:03 +0900

    Title: 특정 문자의 유무, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251017/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,11 @@
+sentence = input()
+
+if 'ee' in sentence:
+    print('Yes', end=' ')
+else:
+    print('No', end=' ')
+
+if 'ab' in sentence:
+    print('Yes', end=' ')
+else:
+    print('No', end=' ')
