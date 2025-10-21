7d4a248292314224da6face9d23de2d73177ce24
Bae Soyeon <flickerlight14@naver.com>
2025-10-21 11:55:52 +0900

    Title: 개인정보, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251021/개인정보/personal-info.py
@@ -0,0 +1,29 @@
+class Person:
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+    def __str__(self):
+        return f"{self.name} {self.height} {self.weight}"
+
+n = 5
+people = []
+
+for _ in range(n):
+    name, h, w = input().split()
+
+    person = Person(name, int(h), float(w))
+    people.append(person)
+
+people.sort(key=lambda person : person.name)
+print("name")
+for i in range(n):
+    print(people[i])
+
+print()
+
+people.sort(key=lambda person : -person.height)
+print("height")
+for i in range(n):
+    print(people[i])
