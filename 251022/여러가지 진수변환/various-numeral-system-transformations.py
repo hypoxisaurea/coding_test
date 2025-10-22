260249e19f80948da3d17c0fba60e7f4a2998163
Bae Soyeon <flickerlight14@naver.com>
2025-10-22 12:00:52 +0900

    Title: 여러가지 진수변환, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251022/여러가지 진수변환/various-numeral-system-transformations.py
+++ 251022/여러가지 진수변환/various-numeral-system-transformations.py
@@ -2,15 +2,12 @@ N, B = map(int, input().split())
 digits = []
 
 while True:
-    if N < 2:
+    if N < B:
         digits.append(N)
         break
 
     digits.append(N % B)
     N //= B
-
-if digits[-1] == 0:
-    digits.pop(-1)
 
 for digit in digits[::-1]:
     print(digit, end='')
