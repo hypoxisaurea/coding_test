bca4e4906c6179b4da8fcee3ca14108ccebb24c0
Bae Soyeon <flickerlight14@naver.com>
2025-10-22 12:54:16 +0900

    Title: 블럭쌓는 명령2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251022/블럭쌓는 명령2/block-stacking-commands2.py
+++ 251022/블럭쌓는 명령2/block-stacking-commands2.py
@@ -2,7 +2,7 @@ n, k = map(int, input().split())
 commands = [tuple(map(int, input().split())) for _ in range(k)]
 
 arr = [
-    0 for _ in range(n)
+    0 for _ in range(n + 1)
 ]
 
 for a, b in commands:
