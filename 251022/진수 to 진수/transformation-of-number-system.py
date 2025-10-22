8cc68903852948119aca910d3e7d09ae996790e9
Bae Soyeon <flickerlight14@naver.com>
2025-10-22 12:42:27 +0900

    Title: 진수 to 진수, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251022/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,19 @@
+a, b = map(int, input().split())
+n = list(map(int, input()))
+length = len(n)
+
+number = 0
+for i in range(length):
+    number = number * a + n[i]
+
+digits = []
+while True:
+    if number < 2:
+        digits.append(number)
+        break
+    
+    digits.append(number % b)
+    number //= b
+
+for digit in digits[::-1]:
+    print(digit, end='')
