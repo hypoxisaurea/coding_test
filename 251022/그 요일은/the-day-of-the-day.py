298b98870fd84c63c968616471928dc3b82043b0
Bae Soyeon <flickerlight14@naver.com>
2025-10-22 10:56:32 +0900

    Title: 그 요일은, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251022/그 요일은/the-day-of-the-day.py
+++ 251022/그 요일은/the-day-of-the-day.py
@@ -4,27 +4,9 @@ A = input()
 day_of_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 day_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
+diff = sum(day_of_month[1:m2]) + d2 - (sum(day_of_month[1:m1]) + d1)
+number = diff - day_of_week.index(A)
 
-def num_of_days(month, day):
-    total_days = 0
+cnt = (number + 7) // 7
 
-    for i in range(1, month):
-        total_days += day_of_month[i]
-    
-    total_days += day
-
-    return total_days
-
-
-diff = num_of_days(m2, d2) - num_of_days(m1, d1)
-if diff < 0:
-    diff += 7
-
-today = diff % 7
-
-count = 0
-for i in range(diff + 1):
-    if A == day_of_week[(today + i) % 7]:
-        count += 1
-
-print(count)
+print(cnt)
