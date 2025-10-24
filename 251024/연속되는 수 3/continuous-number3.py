8ac223bafc3b958965c02357d49289e133f66fdc
Bae Soyeon <flickerlight14@naver.com>
2025-10-24 11:53:39 +0900

    Title: 연속되는 수 3, Time: 56ms, Memory: 16MB, Status: Passed - Codetree

--- 251024/연속되는 수 3/continuous-number3.py
+++ 251024/연속되는 수 3/continuous-number3.py
@@ -4,8 +4,8 @@ arr = [int(input()) for _ in range(N)]
 count = 0
 answer = 0
 
-for i in range(1, len(arr)):
-    if arr[i - 1] * arr[i] > 0:
+for i in range(N):
+    if i >= 1 and arr[i - 1] * arr[i] > 0:
         count += 1
     else:
         count = 1
